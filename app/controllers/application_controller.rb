ae13479c6cc542ae1c71db0f1fb083d2c3e338eb
Dan Williams <dan@ithium.net>
2020-06-23 15:12:34 +0100

    Include helpers in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   include HomeHelper
+  include ActionView::Helpers::SanitizeHelper
+  include FormFlowHelper
 end
