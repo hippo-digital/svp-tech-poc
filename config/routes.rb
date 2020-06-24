f2232a8cf539fcc009506f3759b84163ad3f1ed2
Dan Williams <dan@ithium.net>
2020-06-24 11:56:07 +0100

    Standardised urls to use dashes
    This gets the change buttons on the check your answers page
    working correctly

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   get 'address' => 'address#show'
   post 'address' => 'address#submit'
 
-  get 'help_with_food_delivery' => 'help_with_food_delivery#show'
-  post 'help_with_food_delivery' => 'help_with_food_delivery#submit'
+  get 'help-with-food-delivery' => 'help_with_food_delivery#show'
+  post 'help-with-food-delivery' => 'help_with_food_delivery#submit'
 
   root 'home#index'
 
   get 'user-details' => 'home#user_details'
-  get 'check_your_answers' => 'check_your_answers#show'
+  get 'check-your-answers' => 'check_your_answers#show'
   get '/logout' => 'oidc#logout'
 
   get 'auth/oidc', as: 'authentication'
