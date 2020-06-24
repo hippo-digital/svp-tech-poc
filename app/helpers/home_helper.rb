9c72cb800b51f62e408575fb88cec427a6d6ffb0
Dan Williams <dan@ithium.net>
2020-06-24 12:57:12 +0100

    Remove unused userdetails helper + view

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module HomeHelper
   def user_signed_in?
     session['userinfo'].present?
@@ -9,20 +11,5 @@ module HomeHelper
     else
       redirect_to root_path
     end
-  end
-
-  def userinfo_items
-    user_info_details = @current_user.map do |k, v|
-      {
-          field: k,
-          value: v
-      }
-    end
-
-    user_info_details.compact
-  end
-
-  def reset_session
-    session['userinfo'] = nil if session['userinfo'].present?
   end
 end
