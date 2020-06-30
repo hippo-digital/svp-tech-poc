2403ed2e9743625a3c2ef73a670a95368618861c
Dan Williams <dan@ithium.net>
2020-06-30 11:53:04 +0100

    Added session population with saved answers

--- app/controllers/oidc_controller.rb
+++ app/controllers/oidc_controller.rb
@@ -3,13 +3,23 @@
 class OidcController < ApplicationController
   def callback
     reset_session
+    sub = request.env['omniauth.auth']['extra']['raw_info']['sub']
+    saved_answer = FormResponse.where(NHSSub: sub).first
+    if saved_answer
+      session[:saved_answer] = true
+      session.update(saved_answer.FormResponse)
+      redirect_to check_your_answers_url
+    else
+      session[:saved_answer] = false
+      redirect_to address_url
+    end
+
     session[:auto_generated_data] = {
       example_1: 'Lorem ipsum est...',
       example_2: 'Something or other'
     }
-    session[:userinfo] = request.env['omniauth.auth'].info
-    session[:nhs_sub] = request.env['omniauth.auth']['extra']['raw_info']['sub']
-    redirect_to address_url
+    session[:nhs_sub] =
+      session[:userinfo] = request.env['omniauth.auth'].info
   end
 
   def failure; end
